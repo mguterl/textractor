c012be6c1e372e6c84d7cbaf1211ba8587ac1642
Michael Guterl <mguterl@gmail.com>
2010-11-06 15:34:05 -0400

    fix misplaced freeze, wtf

--- lib/textractor/extractors/docx_extractor.rb
+++ lib/textractor/extractors/docx_extractor.rb
@@ -2,7 +2,7 @@ module Textractor::Extractors
 
   class DocxExtractor
 
-    DEFAULT_DOCX2TXT_PATH = File.expand_path(File.dirname(__FILE__) + "/../../../vendor/docx2txt/docx2txt.pl".freeze)
+    DEFAULT_DOCX2TXT_PATH = File.expand_path(File.dirname(__FILE__) + "/../../../vendor/docx2txt/docx2txt.pl").freeze
 
     class << self
       attr_writer :docx2txt_path
