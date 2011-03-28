dedb6ce0df8c826130f5a303b96ca92795227aed
Brian Michalski <bmichalski@gmail.com>
2011-03-28 14:50:38 -0400

    Revert "Pass file names along as strings to their respective extractors.  This allows filenames with spaces to still be used."
    This reverts commit 863616d43f58ec3805d67a4618478398528c02c9.

--- lib/textractor/extractors/pdf_extractor.rb
+++ lib/textractor/extractors/pdf_extractor.rb
@@ -3,7 +3,7 @@ module Textractor::Extractors
   class PDFExtractor
 
     def text_from_path(path)
-      `pdftotext '#{path}' - 2>/dev/null`.strip
+      `pdftotext #{path} - 2>/dev/null`.strip
     end
 
   end
